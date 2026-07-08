bb366d99871266d35b72bccf044fa35335718422
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-07-09 03:40:25 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (38.15%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,21 +2,27 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-
-        for(char c : s) {
-            if(c == '(' || c == '{' || c == '[') {
-                st.push(c);
-            } else {
-                if(st.empty()) return false;
-                if((c == ')' && st.top() != '(') ||
-                   (c == '}' && st.top() != '{') ||
-                   (c == ']' && st.top() != '[')) {
-                    return false;
-                }
-                st.pop();
-            }
+    
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+            st.push(s[i]);
         }
-
-        return st.empty();
+        else{
+            if(st.empty()){
+                return false;
+            }
+            else if(s[i]==')' && st.top()!='('){
+                return false;
+            }
+            else if(s[i]==']' && st.top()!='['){
+                return false;
+            }
+            else if(s[i]=='}' && st.top()!='{'){
+                return false;
+            }
+            st.pop();
+        }
+    }
+    return st.empty();
     }
 };
