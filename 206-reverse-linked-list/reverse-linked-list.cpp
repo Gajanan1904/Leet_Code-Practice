68daa177c767172c45fd6a2eb31d326b0d1c491b
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-07-11 23:14:17 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (41.56%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -14,7 +14,7 @@ public:
         ListNode *prev=NULL;
         ListNode *current=head;
         while(current!=NULL){
-            ListNode* next=current->next;
+            ListNode *next=current->next;
             current->next=prev;
             prev=current;
             current=next;
