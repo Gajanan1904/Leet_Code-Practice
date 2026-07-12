f78fc4b7da54c1d57ca3ceac604c3bff72f3e706
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-07-13 00:03:57 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (54.19%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode *slow=head;
+        ListNode *fast=head;
+
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        ListNode *second=slow->next;
+        slow->next=NULL;
+
+        ListNode *curr=second;
+        ListNode *prev=NULL;
+
+        while(curr!=NULL){
+            ListNode *next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        second=prev;
+        ListNode *first=head;
+
+        while(first!=NULL && second!=NULL){
+            ListNode *next1=first->next;
+            ListNode *next2=second->next;
+
+            first->next=second;
+            second->next=next1;
+
+            first=next1;
+            second=next2;
+        }
+    }
+};
